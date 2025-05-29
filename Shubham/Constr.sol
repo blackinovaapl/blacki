83d48a8b2eafb9ed6c2f83f56a8237c973a2f52f
blackinovaapl <blackinovaashas@gmail.com>
2025-05-29 11:03:00 +0530

    commit

---
+++ Shubham/Constr.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >= 0.7.0 <0.9.0;
+
+contract love{
+
+    uint public count;
+    constructor(uint new_count){
+        count=new_count;
+    }
+
+}
